83791be326aa7df5a6cef18185c708d53266f65c
Anamika <anamikap6461@gmail.com>
2024-10-30 22:38:18 +0530

    icon and password visibility

---
+++ public/js/togglePassword.js
@@ -0,0 +1,10 @@
+
+
+const passwordInput = document.getElementById('passwordInput');
+const togglePassword = document.getElementById('togglePassword');
+
+togglePassword.addEventListener('click', function () {
+    const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordInput.setAttribute('type', type);
+    this.setAttribute('name', type === 'password' ? 'eye-outline' : 'eye-off-outline');
+});
